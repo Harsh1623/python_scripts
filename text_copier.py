1eb327969888dfb7400f298b025484ca00a08920
Harsh Sharma <58470901+Harsh1623@users.noreply.github.com>
2020-05-19 17:41:23 +0530

    copy from text files in a folder to a new text file

--- text_copier.py
+++ text_copier.py
@@ -1,14 +1,20 @@
 import os
 file_paths = []
-for r,d,f in os.walk(r"E:\ASP_New\EncryptYaraRulesFileApp\Debug\encrypted_rules"):
-    for file in f:
-        if ".yar" in file:
-            file_paths.append(os.path.join(r,file))
+#list of all text files inside a folder
 
+input_path = input("enter folder path to copy from all text files inside it : \n")
+output_path = input("enter a folder path to create txt file in it : \n")
+
+for r,d,f in os.walk(input_path):
+    for file in f:
+        if ".txt" in file:
+            file_paths.append(os.path.join(r,file))
 for f_p in file_paths :
-    with open(f_p, "r") as in_file:
+    with open(f_p, "r",encoding='utf8') as in_file:
         data = in_file.read()
-        write_file = open(r"E:\ASP_New\EncryptYaraRulesFileApp\all_rules.yar","a")
+        write_file = open(output_path+"\\write_file.txt","a")
+        #file will be created with name "write_file.txt"
         write_file.write(data +"\n")
         write_file.close()
     in_file.close()
+    
